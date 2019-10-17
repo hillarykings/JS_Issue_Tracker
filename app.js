48fa0b6ac97d4f1cb12826a572e75ab8d6530e5d
Hillary Peters <hillarypeters233@gmail.com>
2019-10-17 21:19:42 +0100

    added JS file and issues tracking features

---
+++ app.js
@@ -0,0 +1,58 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+const saveIssue = (e) => {
+    let issueDesc = document.getElementById('issueDescInput').value;
+    let issueSeverity = document.getElementById('issueSeverityInput').value;
+    let issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    let issueId = chance.guid();
+    let issueStatus = 'open';
+
+    let issue = {
+        id: issueId,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+    if (localStorage.getItem('issues') == null) {
+        let issues = [];
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+        let issues = JSON.parse(localStorage.getItem('issues'));
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    }
+
+    document.getElementById('issueInputForm').reset();
+    fetchIssues();
+
+    e.preventDefault();
+}
+
+const fetchIssues = () => {
+    let issues = JSON.parse(localStorage.getItem('issues'));
+    let issuesList = document.getElementById('issuesList');
+
+    issuesList.innerHTML = '';
+
+    for (let i = 0; i < issues.length; i++) {
+        let id = issues[i].id;
+        let desc = issues[i].description;
+        let severity = issues[i].severity;
+        let assignedTo = issues[i].assignedTo;
+        let status = issues[i].status;
+
+        issuesList.innerHTML += '<div class="well">'+ 
+                                '<h6>Issue ID: ' + id + '</h6>'+
+                                '<p><span class="label label-info">' + status + '</span></p>'+
+                                '<h3>' + desc + '</h3>'+
+                                '<p><span class="glyhicon glyphicon-time"></span>' + severity + '</p>'+
+                                '<p><span class="glyhicon glyphicon-user"></span>'+ assignedTo + '</p>'+
+                                '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>'+
+                                '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
+                                '</div>';
+
+    }
+}
